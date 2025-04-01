bfc060c1afee5f0df94ca2cd2db1e2ba8e34edd1
lndsilva <lndsilva@hotmail.com>
2025-03-31 22:15:01 -0300

    trabalhando com rotas de grupo

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,27 @@ Route::get('/', [\App\Http\Controllers\PrincipalController::class,'principal'])-
 Route::get('/sobre-nos', [\App\Http\Controllers\SobreNosController::class,'sobreNos'])->name('site.sobre-nos');
 Route::get('/contato', [\App\Http\Controllers\ContatoController::class,'contato'])->name('site.contato');
 
+Route::prefix("/api")->group(function(){
+    Route::get('/produtos',function(){
+        return 'Produtos';    
+    });
+    Route::get('/fornecedores',function(){
+        return 'Fornecedores';    
+    });
+    Route::get('/clientes',function(){
+        return 'Clientes';    
+    });    
+});
+
+Route::get('/login',function(){
+    return 'Login';    
+});
+
+
+
+Route::fallback(function(){
+    echo 'A rota acessada não existe. 
+    <a href="'.route('site.principal').'">Clique aqui</a> 
+    para ir para página inicial';
+});
+
